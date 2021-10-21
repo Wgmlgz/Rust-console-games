442904f28c42e1b718dde4275d2eb8463420dcd5
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2021-10-22 00:09:43 +0300

    doom engine

--- src/doom.rs
+++ src/doom.rs
@@ -1,124 +1,212 @@
 use console_engine::{pixel, ConsoleEngine, KeyCode};
-// use std::{thread, time};
+use std::{f32::consts::PI, time::Instant};
 
 pub const W: u32 = 20;
 pub const H: u32 = 20;
 
-pub const SCREEN_W: u32 = 50;
-pub const SCREEN_H: u32 = 25;
+pub const SCREEN_W: u32 = 150;
+pub const SCREEN_H: u32 = 40;
 
-pub struct Entity {
+#[derive(Default, Clone, Copy)]
+pub struct V2 {
     x: f32,
     y: f32,
+}
+
+impl V2 {
+    pub fn move_polar(&mut self, dist: f32, ang: f32) {
+        self.x += ang.cos() * dist;
+        self.y += ang.sin() * dist;
+    }
+}
+
+#[derive(Default)]
+pub struct Entity {
+    pos: V2,
+    ang: f32,
     badge: char,
     name: String,
 }
 
-pub struct Map {
+impl Entity {
+    fn render(&self, engine: &mut ConsoleEngine) {
+        engine.set_pxl(
+            (self.pos.x.round() * 2.) as i32,
+            self.pos.y.round() as i32 + 20,
+            pixel::pxl(self.badge),
+        );
+    }
+}
+
+#[derive(Default)]
+pub struct Creature {
+    entity: Entity,
+    vel: f32,
+}
+
+impl Creature {
+    fn move_ang(&mut self, dt: f32, ang: f32) {
+        let ang = ang + self.entity.ang;
+        let dir = self.vel * dt;
+
+        self.entity.pos.x += ang.cos() * dir;
+        self.entity.pos.y += ang.sin() * dir;
+    }
+    fn rotate(&mut self, dt: f32, ang: f32) {
+        self.entity.ang += ang * dt;
+    }
+}
+
+struct Map {
     w: u32,
     h: u32,
-    data: String,
-    entities: Vec<Box<Entity>>,
+    world: String,
+    player: Creature,
 }
 
 impl Map {
-    pub fn new(w: u32, h: u32, v: &Vec<&str>) -> Map {
+    fn new(w: u32, h: u32, v: &Vec<&str>) -> Map {
         let mut s = "".to_owned();
         for &line in v {
             s.push_str(line);
         }
-        return Map {
+        let mut map = Map {
             w,
             h,
-            data: s,
-            entities: vec![],
+            world: s,
+            player: Creature::default(),
         };
+        map.player.entity.badge = 'P';
+        map.player.entity.name = String::from("Player");
+        map.player.vel = 10.;
+        map.player.entity.pos.x = 3.;
+        map.player.entity.pos.y = 3.;
+        map
     }
-    pub fn get_pxl(&self, x: u32, y: u32) -> char {
-        let pos = (y * self.w + x) as usize;
-        return if pos < self.data.len() {
-            self.data.as_bytes()[pos] as char
+    fn get_pxl(&self, x: i32, y: i32) -> char {
+        let pos = y * self.w as i32 + x;
+        if pos < 0 {
+            return '^';
+        }
+        let pos = pos as usize;
+        return if pos < self.world.len() {
+            self.world.as_bytes()[pos] as char
         } else {
-            ' '
+            '^'
         };
     }
-    pub fn render(&self, engine: &mut ConsoleEngine) {
-        for y in 0..self.h {
-            for x in 0..self.w {
-                engine.set_pxl((x * 2) as i32, y as i32, pixel::pxl(self.get_pxl(x, y)));
-                engine.set_pxl((x * 2 + 1) as i32, y as i32, pixel::pxl(' '));
+    fn render(&self, engine: &mut ConsoleEngine) {
+        engine.fill_rect(
+            0,
+            (engine.get_height() / 2) as i32,
+            engine.get_width() as i32,
+            (engine.get_height()) as i32,
+            pixel::pxl('`'),
+        );
+
+        let fov = PI / 2.5;
+        let midpx = (engine.get_height() / 2) as i32;
+        for x in 0..engine.get_width() as i32 {
+            let ang =
+                self.player.entity.ang - fov / 2. + fov / engine.get_width() as f32 * x as f32;
+            let mut dist: f32 = 0.;
+            let step = 0.05;
+
+            let mut cur = self.player.entity.pos.clone();
+            while self.get_pxl(cur.x.round() as i32, cur.y.round() as i32) == ' ' {
+                cur.move_polar(step, ang);
+                dist += step;
+            }
+            let bar_sz = ((1. / dist).atan() * 12.).round() as i32;
+            let ch_hit = self.get_pxl(cur.x.round() as i32, cur.y.round() as i32);
+            engine.line(x, midpx - bar_sz, x, midpx + bar_sz, pixel::pxl(ch_hit));
+        }
+    }
+    fn render_map(&self, engine: &mut ConsoleEngine) {
+        for y in 0..self.h as i32 {
+            for x in 0..self.w as i32 {
+                engine.set_pxl((x * 2) as i32, y + 20, pixel::pxl(self.get_pxl(x, y)));
+                engine.set_pxl((x * 2 + 1) as i32, y + 20, pixel::pxl(' '));
             }
         }
-        for entity in self.entities.iter() {
-            engine.set_pxl(
-                (entity.x.round() * 2.) as i32,
-                entity.y.round() as i32,
-                pixel::pxl(entity.badge),
-            );
-        }
+        self.player.entity.render(engine);
     }
-    pub fn push_entity(&mut self, entity: Box<Entity>) {
-        self.entities.push(entity);
-        // let entity: Entity {
-        //     x: 15,
-        //     y: 15,
-        //     badge: '@',
-        //     name: "Player".to_string(),
-        // }
+    fn render_help(&self, engine: &mut ConsoleEngine) {
+        engine.print(0, 0, "wasd: move, qe: rotate, f: map, h: help, x: exit")
     }
 }
 
 pub fn run(engine: &mut ConsoleEngine) {
     let v = vec![
-        ".......YYY..........",
-        ".......YYY..........",
-        ".......YYY..........",
-        "....................",
-        "....................",
-        "....................",
-        "...XXXXXXXX.........",
-        "...XXXXXXXX.........",
-        "...XXXXXXXX.........",
-        "...XXXXXXXX.........",
-        "...XXXXXXXX.........",
-        "...XXXXXXXX.........",
-        "....................",
-        "....................",
-        "....................",
-        "....................",
-        "....................",
-        "....................",
-        "....................",
-        "....................",
+        "#=#=#=#=#=#=#=#=#=#=",
+        "#    #             #",
+        "=    =        #    =",
+        "#    #        =    #",
+        "=      #=#    #    =",
+        "#           #=#    #",
+        "=    #=#=          =",
+        "#    =             #",
+        "=    #    #        =",
+        "#         =#=#     #",
+        "=         #        =",
+        "#                  #",
+        "=    #      #=#    =",
+        "#    =             #",
+        "=    #   #=#       =",
+        "#           #=#    #",
+        "=                  =",
+        "#   #=#    #=#     #",
+        "=                  =",
+        "#=#=#=#=#=#=#=#=#=#=",
     ];
 
     let mut map = Map::new(W, H, &v);
 
-    let player = Box::new(Entity {
-        x: 2.,
-        y: 2.,
-        badge: '@',
-        name: "bro".to_string(),
-    });
-
-    let prikol = Box::new(Entity {
-        x: 15.,
-        y: 15.,
-        badge: '2',
-        name: "prikol".to_string(),
-    });
-
-    map.push_entity(player);
-    map.push_entity(prikol);
-
     engine.clear_screen();
+
+    let mut minimap = false;
+    let mut help = false;
+
     loop {
+        let now = Instant::now();
         engine.wait_frame();
         engine.clear_screen();
-        map.render(engine);
 
+        let dt = now.elapsed().as_secs_f32();
+
+        if engine.is_key_pressed(KeyCode::Char('w')) {
+            map.player.move_ang(dt, 0.);
+        }
+        if engine.is_key_pressed(KeyCode::Char('a')) {
+            map.player.move_ang(dt, -PI / 2.);
+        }
+        if engine.is_key_pressed(KeyCode::Char('s')) {
+            map.player.move_ang(dt, PI);
+        }
+        if engine.is_key_pressed(KeyCode::Char('d')) {
+            map.player.move_ang(dt, PI / 2.);
+        }
         if engine.is_key_pressed(KeyCode::Char('q')) {
+            map.player.rotate(dt, -10.);
+        }
+        if engine.is_key_pressed(KeyCode::Char('e')) {
+            map.player.rotate(dt, 10.);
+        }
+        if engine.is_key_pressed(KeyCode::Char('x')) {
             break;
+        }
+        if engine.is_key_pressed(KeyCode::Char('f')) {
+            minimap ^= true;
+        }
+        if engine.is_key_pressed(KeyCode::Char('h')) {
+            help ^= true;
+        }
+        map.render(engine);
+        if minimap {
+            map.render_map(engine);
+        }
+        if help {
+            map.render_help(engine);
         }
 
         engine.draw();
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use console_engine::ConsoleEngine;
 
 mod doom;
 fn main() {
-    let mut engine = ConsoleEngine::init(doom::SCREEN_W, doom::SCREEN_H, 3).unwrap();
+    let mut engine = ConsoleEngine::init(doom::SCREEN_W, doom::SCREEN_H, 60).unwrap();
     doom::run(&mut engine);
 }
